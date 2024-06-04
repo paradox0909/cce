68a741d92295bbe3cacce2f7ce1317e5f9c7983e
lizparadox_ <85692552+paradox0909@users.noreply.github.com>
2024-06-05 05:21:52 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-apt-get update
-        echo "apt-get has updated."
+#apt-get update
+	echo "apt-get has updated."
+#apt-get install libpam-pwquality -y
+#echo "libpam-pwquality 패키지가 설치되었습니다. 작업을 진행합니다."
 #1-1-1 telnet check
 check_telnet=$(telnet 127.0.0.1 2>/dev/null)
 if [ -n "$check_telnet" ]; then
@@ -88,9 +90,6 @@ else
 fi
 
 #1-3 Set account lockout settings
-apt-get install libpam-pwquality -y
-apt-get install libpam-pwquality -y
-echo "libpam-pwquality 패키지가 설치되었습니다. 작업을 진행합니다."
 
 account_lockout_setting_path="/etc/pam.d/system-auth"
 account_lockout_setting_pattern="auth required /lib/security/pam_tally.so deny=5 unlock_time=120
@@ -110,3 +109,109 @@ else
 fi
 echo "1-3 계정 잠금 임계값 설정 시, /lib/security/pam_tally.so 파일이 있는지 확인 필요. 해당 라이브러리가 실제 존재하는지 확인 필요."
 #1-4 password file protect
+if [ -f /etc/passwd ]; then
+    echo "디렉토리가 존재합니다."
+
+    while IFS=: read -r etc_passwd_user etc_passwd_pass etc_passwd_rest; do
+        # 두 번째 필드가 "x"인지 확인
+        if [ "$etc_passwd_pass" = "x" ]; then
+            echo "정상: $etc_passwd_user"
+        else
+            echo "수동 진단 요망: $etc_passwd_user"
+        fi
+    done < /etc/passwd
+
+else
+    echo "etc/passwd 디렉토리 수동 확인 요망"
+fi
+
+#2-1 root홈 패스 디렉터리 권한 및 패스 설정
+
+root_home_path_authority=$(which ls)
+
+if [ "$root_home_path_authority" != "${root_home_path_authority#*.}" ] || [ "$root_home_path_authority" != "${root_home_path_authority%:*}" ]; then
+    echo "취약: PATH 환경변수에 '.'이 맨 앞이나 중간에 포함되어 있습니다.수동 확인 요망."
+else
+    echo "양호: PATH 환경변수에 '.'이 맨 앞이나 중간에 포함되지 않았습니다."
+fi
+
+#2-2 파일 및 디렉터리 소유자 설정
+
+files_without_owner=$(find / -nouser -o -nogroup -print 2>/dev/null)
+count_files_without_owner=$(echo "$files_without_owner" | wc -l)
+
+if [ "$count_files_without_owner" -eq 0 ]; then
+    echo "양호 : 소유자가 존재하지 않는 파일 및 디렉터리가 존재하지 않습니다."
+else
+    echo "취약 : 소유자가 존재하지 않는 파일 및 디렉터리가 존재합니다."
+    echo " 소유자가 존재하지 않는 디렉터리 목록: $files_without_owner"
+fi
+
+#2-3 /etc/passwd 파일 소유자 및 권한 설정
+
+passwd_file_path="/etc/passwd"
+file_owner_name=$(stat -c %U "$passwd_file_path")
+file_permissions=$(stat -c %a "$passwd_file_path")
+
+required_owner="root"
+required_permissions="644"
+
+if [ "$file_owner_name" = "$required_owner" ] && [ "$file_permissions" -eq "$required_permissions" ]; then
+    echo "양호 : /etc/passwd 파일의 소유자가 root이고, 권한이 644입니다."
+else
+    echo "취약 : /etc/passwd 파일의 소유자가 root가 아니거나, 권한이 644 이하가 아닙니다. 현재 소유자: $file_owner_name, 현재 권한 : $file_permissions"
+fi
+
+#2-4 /etc/shadow 파일 소유자 및 권한
+
+shadow_file_path="/etc/shadow"
+shadow_file_owner=$(stat -c %U "$shadow_file_path")
+shadow_file_permissions=$(stat -c %a "$shadow_file_path")
+
+required_owner="root"
+required_permissions="400"
+
+if [ "$shadow_file_owner" = "$required_owner" ] && [ "$shadow_file_permissions" -le "$required_permissions" ]; then
+	echo "양호 : /etc/shadow 파일의 소유자가 root이고, 권한이 400 이하입니다."
+else
+	chown root "$shadow_file_path"
+	chmod 400 "$shadow_file_path"
+	echo "조치 완료"
+fi
+
+#2-5 /etc/hosts 파일 소유자 및 권한 설정
+hosts_file_path="/etc/hosts"
+hosts_file_owner=$(stat -c %U "$hosts_file_path")
+hosts_file_permissions=$(stat -c %a "$hosts_file_path")
+
+host_required_owner="root"
+host_required_permissions="600"
+
+if [ "$hosts_file_owner" = "$host_required_owner" ] && [ "$hosts_file_permissions" -le "$host_required_permissions" ]; then
+	echo "양호 : /etc/hosts 파일의 소유자가 root이고, 권한이 600 이하입니다."
+else
+	echo "취약 : 현재 소유자 : $hosts_file_owner, 현재 권한 : $hosts_file_permissions"
+	chown root "$hosts_file_path"
+	chmod 600 "$hosts_file_path"
+	echo "파일의 소유자를 root로, 권한을 600으로 변경하였습니다."
+fi
+
+#2-6 /etc/(x)inetd.conf 파일 소유자 및 권한 설정
+
+inetd_conf_file="/etc/inetd.conf"
+xinetd_d_directory="/etc/xinetd.d/"
+
+inetd_conf_file_owner=$(stat -c %U "$inetd_conf_file")
+inetd_conf_file_permissions=$(stat -c %a "$inetd_conf_file")
+
+inetd_required_owner="root"
+inetd_required_permissions="600"
+
+if [ "$inetd_conf_file_owner" = "$inetd_required_owner" ] && [ "$inetd_conf_file_permissions" -eq "$inetd_required_permissions" ]; then
+	echo "양호 : /etc/inetd.conf 파일의 소유자가 root이고, 권한이 600입니다."
+else
+	echo "취약 : 현재 소유자 : $inetd_conf_file_owner 현재 권한 : $inetd_conf_file_permissions"
+	chown root "$inetd_conf_file"
+	chmod 600 "$inetd_conf_file"
+	echo "조치 완료. /etc/inetd.conf 파일의 소유자를 root로, 권한을 600으로 변경하였습니다."
+fi
